23f98b72afc454e0cb05af2ca7ecaca6445ad465
Daniel Aguirre <ddaguirre99@outlook.com>
2023-03-14 12:27:05 -0500

    products were mappped on the menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import CategoriaContainer from '../Categoria'
+import ProductCard from '../Producto'
 
 function Menu() {
     return (
         <div>
             <CategoriaContainer title="Bebidas">
-                <div>Bebida 1</div>
-                <div>Bebida 2</div>
-                <div>Bebida 3</div>
+                <ProductCard title="Capuccino" imageSrc="https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/10/bebidas-alcoholicas-que-se-pueden-mezclar-con-cafe.jpg" price={15000} />
+                <ProductCard title="Frape" imageSrc="https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/10/bebidas-alcoholicas-que-se-pueden-mezclar-con-cafe.jpg" price={13000} />
+                <ProductCard title="Mocaccino" imageSrc="https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/10/bebidas-alcoholicas-que-se-pueden-mezclar-con-cafe.jpg" price={17000} />
             </CategoriaContainer>
             <CategoriaContainer title="Alimentos">
-                <div>Alimento 1</div>
-                <div>Alimento 2</div>
-                <div>Alimento 3</div>
+                <ProductCard title="Waffles" imageSrc="https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2019/09/04093356/RFB-2708-1-tortafriadeguanabana.jpg" price={25000} />
+                <ProductCard title="Pai de limon" imageSrc="https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2019/09/04093356/RFB-2708-1-tortafriadeguanabana.jpg" price={33000} />
+                <ProductCard title="Torta Fria" imageSrc="https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2019/09/04093356/RFB-2708-1-tortafriadeguanabana.jpg" price={42000} />
             </CategoriaContainer>
         </div>
     )
