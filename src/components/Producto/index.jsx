2a15145c5eb6ff6ccf3695e29b74891f8ac8b467
Jessica <jessica.gomez.7281@miremington.edu.co>
2023-03-14 02:21:35 -0500

    se creo el componente de Productos

---
+++ src/components/Producto/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material'
+
+function ProductCard({ imageSrc, title, price, onAdd }) {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image={imageSrc}
+                alt={title}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography variant="h6" color="text.secondary">
+                    $ {price}
+                </Typography>
+            </CardContent>
+            <Button onClick={onAdd} variant="contained">
+                Agregar
+            </Button>
+        </Card>
+    )
+}
+
+export default ProductCard
