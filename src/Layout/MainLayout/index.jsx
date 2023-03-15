4e907a7752ce5afb59028e5ce765cc7f921ca7d4
Giovanny Montaña <gmm.developer@gmail.com>
2023-03-14 20:32:16 -0500

    se agrego el footer al layout

--- src/Layout/MainLayout/index.jsx
+++ src/Layout/MainLayout/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
-import { Container, AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
+import { Container, AppBar, Toolbar, Typography, Button, Box, Grid, Link, Icon } from '@mui/material'
+import { Facebook, Instagram, Twitter, WhatsApp } from '@mui/icons-material';
 import { Outlet, useNavigate } from 'react-router-dom'
-
-
 
 function Layout({ children }) {
     const navigate = useNavigate()
+
     return (
         <>
             <AppBar position="static" sx={{ backgroundColor: '#f4f4f4' }}>
@@ -25,6 +25,49 @@ function Layout({ children }) {
             <Container>
                 <Outlet />
             </Container>
+            <Container maxWidth="md">
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} md={4}>
+                        <Typography variant="subtitle1" component="p">
+                            Horario de Atención:
+                        </Typography>
+                        <Typography variant="body1" component="p">
+                            Lunes a Viernes de 8 a.m. a 10 p.m.
+                        </Typography>
+                        <Typography variant="body1" component="p">
+                            Sabados de 8 a.m. a 8 p.m.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={4} container justify="center" justifyContent="space-between">
+                        <Link href="https://www.facebook.com" target="_blank">
+                            <Icon>
+                                <Facebook />
+                            </Icon>
+                        </Link>
+                        <Link href="https://www.instagram.com/" target="_blank">
+                            <Icon>
+                                <Instagram />
+                            </Icon>
+                        </Link>
+                        <Link href="https://twitter.com/" target="_blank">
+                            <Icon>
+                                <Twitter />
+                            </Icon>
+                        </Link>
+                        <Link href="https://www.whatsapp.com/?lang=es" target="_blank">
+                            <Icon>
+                                <WhatsApp />
+                            </Icon>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12} md={4} container justify="flex-end">
+                        <Typography variant="body2" component="p">
+                            Sitio Diseñado "Estudiantes Remigton" logo © 2023 Usuario Contribución
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+            );
         </>
     )
 }
